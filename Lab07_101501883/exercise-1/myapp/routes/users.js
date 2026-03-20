2145938e5d4896c9aba5aff9d226b8964db7552a
karina-vetlugina <karinavetlugina05@gmail.com>
2026-03-19 21:35:41 -0400

    lab wk10

---
+++ Lab07_101501883/exercise-1/myapp/routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+// Middleware
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+// GET route
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// POST route
+router.post('/', function(req, res, next) {
+  console.log('First Name:', req.body.firstname);
+  console.log('Last Name:', req.body.lastname);
+  res.send('POST received!');
+});
+
+module.exports = router;
